20b495908dfb63f61d53a7b6e6625c2c91c1cce5
Haji-Sharifi <mustafasharifi53@icloud.com>
2025-08-23 16:59:19 +0430

    push

--- MyFirstHelloWorld.java
+++ MyFirstHelloWorld.java
@@ -1,5 +1,6 @@
 public class MyFirstHelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println("this is my first java program");
     }
 }
