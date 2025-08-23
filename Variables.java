370999e2a5a4f9a6cb4929ec3af5edf4360ee890
Haji-Sharifi <mustafasharifi53@icloud.com>
2025-08-23 17:25:35 +0430

    using variables

---
+++ Variables.java
@@ -0,0 +1,32 @@
+public class Variables {
+    public static void main(String[] args) {
+
+
+        // Variables= a reusable container for a value
+        // a variable behaves as if it was the value it contains
+
+        // primitives = simple values stored directly in memory (stack)
+        // Reference = memory address (stack) that points to the (heap)
+
+        // primitive vs Refrence 
+        // ---------    --------
+        // int          string
+        // double       array
+        // boolean      object
+        // char         class
+
+        // 2 Steps to create a variable
+        // ----------------------------
+        // 1. Declare the variable
+        // 2. Initialize the variable
+
+
+        int age = 20 ;
+        System.out.println(age);
+
+      
+
+
+    }
+    
+}
