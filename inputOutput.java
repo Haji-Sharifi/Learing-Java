1b407043d8e14021c52eaa1458392d32d51ec719
Haji-Sharifi <mustafasharifi53@icloud.com>
2025-08-24 14:19:25 +0430

    update

--- inputOutput.java
+++ inputOutput.java
@@ -6,16 +6,27 @@ public class inputOutput {
         
 
         System.out.print("Enter your name: ");
-        String name = scanner.nextLine();
+        String name = scanner.nextLine();  // nextLine For string input
 
         System.out.print("Enter your age: ");
-        int age = scanner.nextInt();
+        int age = scanner.nextInt(); // nextInt for integer input
 
         System.out.println("Enter your gpa : ");
-        double gpa = scanner.nextDouble();
+        double gpa = scanner.nextDouble(); // nextDouble for double input
 
-        System.out.println("Hello, " + name + "! You are " + age + " years old." + " Your GPA is " + gpa + ".") ;
+        System.out.println("Are you a student? (true/false): ");
+        boolean isStudent = scanner.nextBoolean(); // nextBoolean for boolean input
+      
 
+
+        System.out.println("Hello, !" + name + ".") ;
+        System.out.println("You are " + age + " years old.");
+        System.out.println(" Your GPA is " + gpa + ".");
+              if (isStudent) {
+            System.out.println("You are a student.");
+        } else {
+            System.out.println("You are not a student.");
+        }
         scanner.close();
     }
 }
