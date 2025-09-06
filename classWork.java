5e720936539d13e2f1baedc7b88072113945ec54
Haji-Sharifi <mustafasharifi53@icloud.com>
2025-09-06 13:07:51 +0430

    added

---
+++ classWork.java
@@ -0,0 +1,82 @@
+
+import java.util.Scanner;
+
+public class classWork {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // reverse of the numbers 
+
+        // System.out.println("reverse number");
+        // System.out.println("Enter a number");
+        // int num = scanner.nextInt();
+        // int rev = 0 ;
+        // while(num != 0 ){
+        //     int degit = num % 10 ;
+        //     rev = rev * 10 + degit ;
+        //     num/=10 ;            
+        // }
+        // System.out.println("Reversed" + " : " + rev );
+
+
+        // sum of degits 
+        // System.out.println("enter a number of the degits addition");
+        // int num = scanner.nextInt(); 
+        // int sumDigits = 0 ;
+        // while (num != 0 ){
+        //     sumDigits += num % 10 ;
+        //     num /=10 ;                    
+        // }
+        // System.out.println("Sum of degits" + " : " + sumDigits );
+
+
+        // palindrome check 
+        // System.out.println("Enter a number to check palindrome ");
+        // int num = scanner.nextInt();
+        // int temp = num , rev = 0 ;
+        // while(temp != 0 ){
+        //   rev = rev * 10 + temp % 10 ;
+        //   temp /= 10 ;
+
+        // }
+        // if (num == rev ){
+        //     System.out.println(num + " Number is palindrome ");
+        // }
+        // else{
+        //     System.out.println(num + " the number is not palindrome ");
+        // }
+
+        // Factorial of a number
+        // System.out.println("Enter a number to find the factorial");
+        // int num = scanner.nextInt();
+        // long fact = 1;
+        // for (int i = 1; i<= num; i++) {
+        //     fact *= i;
+        // }
+        // System.out.println("factorial = " + fact);
+
+
+        // scanner.close();
+
+
+        // 5 4! 3! 2! 1!
+
+        // for (int n = 2; n <= 500; n++) {
+        //     boolean isPrime = true;
+        //     for (int i = 2; i <= Math.sqrt(n); i++) {
+        //         if (n % i ==0){
+        //             isPrime = false;
+        //             break;
+
+        //         }
+        //     }
+        //     if (isPrime) {
+        //         System.out.println(n+"");
+        //     }
+        // }
+
+        scanner.close();
+
+        
+    }
+}
